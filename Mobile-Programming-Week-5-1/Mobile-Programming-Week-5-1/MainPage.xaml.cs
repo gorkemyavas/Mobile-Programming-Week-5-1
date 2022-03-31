e32150a6cdd47068a5d9c143a300e6073b716e16
gorkem <gorkemyavas98@gmail.com>
2022-03-31 09:53:49 +0300

    Add project files.

---
+++ Mobile-Programming-Week-5-1/Mobile-Programming-Week-5-1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace Mobile_Programming_Week_5_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Padding = new Thickness(2);
+            Content = new Frame
+            {
+                BorderColor = Color.Accent,
+                BackgroundColor = Color.Green,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Content = new Label
+                {
+                    Text = "Çerçeve içerisindeki metin !!!",
+                    FontSize = Device.GetNamedSize(NamedSize.Header, typeof(Label)),
+                    FontAttributes = FontAttributes.Bold,
+                    TextColor = Color.White,
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center,
+                    BackgroundColor = Color.Tomato
+                }
+            };
+        }
+    }
+}
